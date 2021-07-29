3cf7fedf31bacbff8e6bbf598f4a3f633357a132
Alexander <example@gmail.com>
2021-07-29 15:11:19 +0600

    1 Step Done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.carousel').slick({
+        speed:600,
         dots:true,
         arrow: true,
         infinite: true,
@@ -7,10 +8,15 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right.png"></button>'
     });
     $('.branches__carousel').slick({
+        speed:600,
         dots:true,
         arrow: true,
         infinite: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arr_left2.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right2.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right2.png"></button>',
+        customPaging : function(slider, i) {
+            var title = $(slider.$slides[i]).data('title');    
+            return '<a class="branches__carousel__item"> '+title+' </a>';
+        }
     });
 });
